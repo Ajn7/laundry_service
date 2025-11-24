a54390f2e9c2cbb20556985de67fb3f57906ea1a
Ajn7 <109603924+Ajn7@users.noreply.github.com>
2025-11-25 00:18:01 +0530

    Add IP address to allowed hosts

--- laundry_service/settings.py
+++ laundry_service/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-_@360lj*ey)+($2b+a!_^tda&x4t(ix1k-d!7#6g)%olc&w)a6
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'harmonious-fulfillment-production-c21d.up.railway.app'
+    'harmonious-fulfillment-production-c21d.up.railway.app',
+    '144.24.54.145'
 ]
 
 
